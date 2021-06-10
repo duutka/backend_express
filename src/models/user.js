3fbc3b9ab8d3f202528f382cbd5f48007742fbc2
troexol <vanyavanya0707@mail.ru>
2021-06-10 21:08:00 +0500

    feat: 🎸 +user table in database +user model

---
+++ src/models/user.js
@@ -0,0 +1,33 @@
+/* OTHER */
+import pool from '../config/db.config.js';
+
+/* Get user by login */
+const findByLogin = (login) =>
+    new Promise(async (resolve, reject) => {
+        try {
+            const user = await pool.query('SELECT * FROM user WHERE login = $1', [login]);
+
+            resolve(user.rows[0]);
+        } catch (error) {
+            reject(error);
+        }
+    });
+
+/* Create new user */
+const add = ({ login, password, firstname, lastname }) =>
+    new Promise(async (resolve, reject) => {
+        try {
+            await pool.query(
+                `INSERT INTO user
+                (login, password, firstname, lastname)
+                VALUES ($1, $2, $3, $4)`,
+                [login, password, firstname, lastname],
+            );
+
+            resolve(true);
+        } catch (error) {
+            reject(error);
+        }
+    });
+
+export default { findByLogin, add };
